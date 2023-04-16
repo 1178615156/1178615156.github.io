12d2988a54daf56bb2fc4f89aceeb6e26aa243ad
yjs <yjs@weizhi.com>
2023-04-16 18:59:48 +0800

    update

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,12 +1,12 @@
 // import {googleAnalyticsPlugin} from '@vuepress/plugin-google-analytics'
-const googleAnalyticsPlugin = require('@vuepress/plugin-google-analytics');
+// const googleAnalyticsPlugin = require('@vuepress/plugin-google-analytics');
 
 module.exports = {
-    plugins: [
-        googleAnalyticsPlugin({
-            id: 'G-K78EX03G2S'
-        })
-    ],
+    plugins: {
+        '@vuepress/plugin-google-analytics': {
+            id: 'G-K78EX03G2S',
+        },
+    },
     title: '',
     description: '',
     theme: '@vuepress/theme-blog',
@@ -21,9 +21,4 @@ module.exports = {
         ],
     }
 
-
-    // themeConfig:{
-    //     navbar: false,
-    //     sidebar: 'auto'
-    // }
 }
